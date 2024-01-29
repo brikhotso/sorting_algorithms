4288b0e96ef1cbb4c8fe2f179af731b222ac7ccf
brikhotso <brenda.rikhotso@gmail.com>
2024-01-29 09:30:17 -0800

    sorting

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,35 +1,69 @@
 #include "sort.h"
 
+void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow);
+void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
+		   char flow);
+void bitonic_sort(int *array, size_t size);
+
+/**
+ * bitonic_seq - Convert an array of integers into bitonic seq.
+ * @array: The array to be merged.
+ * @size: The size of the array.
+ * @flow: Flag to indicate whether the merging is in increasing order.
+ * @start: The starting index of the sequence in array to sort.
+ * @seq: The size of the sequence to sort.
+ */
+void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow)
+{
+	size_t cut = seq / 2;
+	char *str = (flow == UP) ? "UP" : "DOWN";
+
+	if (seq > 1)
+	{
+		printf("Merging [%lu/%lu] (%s):\n", seq, size, str);
+
+		print_array(array + start, seq);
+		bitonic_seq(array, size, start, cut, UP);
+		bitonic_seq(array, size, start + cut, cut, DOWN);
+		bitonic_merge(array, size, start, seq, flow);
+
+		printf("Result [%lu/%lu] (%s):\n", seq, size, str);
+		print_array(array + start, seq);
+	}
+}
+
 /**
  * bitonic_merge - Merges two halves of an array in the desired order.
  * @array: The array to be merged.
  * @size: The size of the array.
- * @up: Flag to indicate whether the merging is in increasing order.
+ * @flow: Flag to indicate whether the merging is in increasing order.
+ * @start: The starting index of the sequence in array to sort.
+ * @seq: The size of the sequence to sort.
  */
-void bitonic_merge(int *array, size_t size, int up)
+void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
+		char flow)
 {
-	size_t i, half;
-	int temp;
-    if (size > 1)
-    {
-        half = size / 2;
+	size_t index, jump = seq / 2;
+	int tmp, *arr1, *arr2;
 
-        for (i = 0; i < half; ++i)
-        {
-            if ((array[i] > array[i + half]) == up)
-            {
-                temp = array[i];
-                array[i] = array[i + half];
-                array[i + half] = temp;
-            }
-        }
-
-	printf("Merging [%lu/%lu] (%s):\n", size, size * 2, up ? "UP" : "DOWN");
-        print_array(array, size * 2);
-	
-        bitonic_merge(array, half, up);
-        bitonic_merge(array + half, half, up);
-    }
+	if (seq > 1)
+	{
+		for (index = start; index < start + jump; index++)
+		{
+			arr1 = array + index;
+			arr2 = array + index + jump;
+			if ((flow == UP && array[index] > array[index + jump])
+			    || (flow == DOWN && array[index] <
+				array[index + jump]))
+			{
+				tmp = *arr1;
+				*arr1 = *arr2;
+				*arr2 = tmp;
+			}
+		}
+		bitonic_merge(array, size, start, jump, flow);
+		bitonic_merge(array, size, start + jump, jump, flow);
+	}
 }
 
 /**
@@ -39,16 +73,8 @@ void bitonic_merge(int *array, size_t size, int up)
  */
 void bitonic_sort(int *array, size_t size)
 {
-    if (size > 1)
-    {
-        size_t half = size / 2;
+	if (array == NULL || size < 2)
+		return;
 
-        printf("Merging [%lu/%lu] (UP):\n", half, size);
-        print_array(array, size);
-
-        bitonic_sort(array, half);
-        bitonic_sort(array + half, half);
-
-        bitonic_merge(array, size, 1);
-    }
+	bitonic_seq(array, size, 0, size, UP);
 }
